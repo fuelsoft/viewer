9b824894176bfc5f9a3a801547d7b198469ae0b9
Nick <fuelsoft@users.noreply.github.com>
2020-04-17 00:45:45 -0700

    Rewrote save and load functions - much simpler to extend now Fixed replace-all mistakes where MAXIMIZED became MOVED

--- main.cpp
+++ main.cpp
@@ -86,18 +86,13 @@ namespace IVCONST {
 
 namespace IVGLOBAL {
 
-	int WINDOW_X = SDL_WINDOWPOS_UNDEFINED;
-	int WINDOW_Y = SDL_WINDOWPOS_UNDEFINED;
 	int WINDOW_PREVIOUS_X = SDL_WINDOWPOS_UNDEFINED;
 	int WINDOW_PREVIOUS_Y = SDL_WINDOWPOS_UNDEFINED;
 
-	int WINDOW_WIDTH = IVCONST::WINDOW_WIDTH_DEFAULT;
-	int WINDOW_HEIGHT = IVCONST::WINDOW_HEIGHT_DEFAULT;
 	int WINDOW_PREVIOUS_WIDTH = IVCONST::WINDOW_WIDTH_DEFAULT;
 	int WINDOW_PREVIOUS_HEIGHT = IVCONST::WINDOW_HEIGHT_DEFAULT;
 
-	bool WINDOW_MAXIMIZED = 0;
-	bool WINDOW_MOVED = 0;
+	bool WINDOW_MOVED = false;
 
 	/* DISPLAY */
 	int IMAGE_WIDTH = 0;
@@ -106,8 +101,6 @@ namespace IVGLOBAL {
 	int VIEWPORT_X = 0;
 	int VIEWPORT_Y = 0;
 	float VIEWPORT_ZOOM = 1.0f;
-
-	bool DISPLAY_MODE_DARK = true;
 
 	SDL_Texture* TEXTURE_TRANSPARENCY_DARK;
 	SDL_Texture* TEXTURE_TRANSPARENCY_LIGHT;
@@ -122,6 +115,23 @@ namespace IVGLOBAL {
 	std::vector<std::filesystem::path> FILES_ADJACENT_IMAGES;
 	uint32_t IMAGE_FILE_INDEX = 0;
 
+	struct IVSETTINGS {
+		int WINDOW_X;
+		int WINDOW_Y;
+		int WINDOW_WIDTH;
+		int WINDOW_HEIGHT;
+		bool WINDOW_MAXIMIZED;
+		bool DISPLAY_MODE_DARK;
+	} SETTINGS;
+
+	struct IVSETTINGS DEFAULTS = {
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		IVCONST::WINDOW_WIDTH_DEFAULT,
+		IVCONST::WINDOW_HEIGHT_DEFAULT,
+		false,
+		true
+	};
 }
 
 /* /// CODE /// */
@@ -133,26 +143,31 @@ void readSettings() {
 	//if CWD is length 0, program was run from terminal and is CWD is unknown. Use default settings.
 	if (IVGLOBAL::PATH_PROGRAM_CWD.empty()) {
 		std::cerr << IVCONST::LOG_NOTICE << "Unable to load settings file. Using defaults." << std::endl;
+		IVGLOBAL::SETTINGS = IVGLOBAL::DEFAULTS; //copy defaults
 		return;
 	}
+	//open filestream to read file at the end
 	std::ifstream inputStream(IVGLOBAL::PATH_PROGRAM_CWD / IVCONST::FILENAME_SETTINGS, std::ifstream::in | std::ifstream::binary | std::ifstream::ate);
 	if (inputStream.is_open()) {
-		if (inputStream.tellg() >= IVCONST::SETTINGDATASIZE) {
-			inputStream.seekg(std::ios_base::beg);
-			inputStream.read(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_X), sizeof(IVGLOBAL::WINDOW_X));
-			inputStream.read(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_Y), sizeof(IVGLOBAL::WINDOW_Y));
-			inputStream.read(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_WIDTH), sizeof(IVGLOBAL::WINDOW_WIDTH));
-			inputStream.read(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_HEIGHT), sizeof(IVGLOBAL::WINDOW_HEIGHT));
-			inputStream.read(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_MAXIMIZED), sizeof(IVGLOBAL::WINDOW_MAXIMIZED));
-			inputStream.read(reinterpret_cast<char *>(&IVGLOBAL::DISPLAY_MODE_DARK), sizeof(IVGLOBAL::DISPLAY_MODE_DARK));
+		//current position is end, this will present filesize
+		int fileLength = inputStream.tellg();
+		if (fileLength == -1) { //read failed somewhere in iostream
+			std::cerr << IVCONST::LOG_NOTICE << "Failed to read settings file!" << std::endl;
+			IVGLOBAL::SETTINGS = IVGLOBAL::DEFAULTS; //copy defaults
 		}
-		else {
-			std::cerr << IVCONST::LOG_WARNING << "Incorrect settings data size!" << std::endl;
+		else if ((unsigned) fileLength < sizeof(IVGLOBAL::SETTINGS)) { //settings file is too short
+			std::cerr << IVCONST::LOG_WARNING << "Incorrect settings data size! [Expected " << sizeof(IVGLOBAL::SETTINGS) << ", got " << fileLength << "]" << std::endl;
+			IVGLOBAL::SETTINGS = IVGLOBAL::DEFAULTS; //copy defaults
+		}
+		else { //no problems encountered
+			inputStream.seekg(std::ios_base::beg);
+			inputStream.read(reinterpret_cast<char *>(&IVGLOBAL::SETTINGS), sizeof(IVGLOBAL::SETTINGS));
 		}
 		inputStream.close();
 	}
 	else { //load defaults
 		std::cerr << IVCONST::LOG_NOTICE << "No settings file located!" << std::endl;
+		IVGLOBAL::SETTINGS = IVGLOBAL::DEFAULTS; //copy defaults
 	}
 }
 
@@ -165,31 +180,17 @@ void writeSettings() {
 	std::ofstream outputStream(IVGLOBAL::PATH_PROGRAM_CWD / IVCONST::FILENAME_SETTINGS, std::ifstream::out | std::ifstream::binary | std::ifstream::trunc);
 	if (outputStream.is_open()) {
 		//if window was 'moved' during resize, discard
-		if (!IVGLOBAL::WINDOW_MAXIMIZED) {
-			outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_PREVIOUS_X), sizeof(IVGLOBAL::WINDOW_PREVIOUS_X));
-			outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_PREVIOUS_Y), sizeof(IVGLOBAL::WINDOW_PREVIOUS_Y));
-		}
-		//if window was actually moved, record it
-		else {
-			outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_X), sizeof(IVGLOBAL::WINDOW_X));
-			outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_Y), sizeof(IVGLOBAL::WINDOW_Y));
+		if (!IVGLOBAL::WINDOW_MOVED) {
+			IVGLOBAL::SETTINGS.WINDOW_X = IVGLOBAL::WINDOW_PREVIOUS_X;
+			IVGLOBAL::SETTINGS.WINDOW_Y = IVGLOBAL::WINDOW_PREVIOUS_Y;
 		}
 		//if the window is maximized, write the last known size before it was maximized
-		if (IVGLOBAL::WINDOW_MAXIMIZED) {
-			outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_PREVIOUS_WIDTH), sizeof(IVGLOBAL::WINDOW_PREVIOUS_WIDTH));
-			outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_PREVIOUS_HEIGHT), sizeof(IVGLOBAL::WINDOW_PREVIOUS_HEIGHT));
+		if (IVGLOBAL::SETTINGS.WINDOW_MAXIMIZED) {
+			IVGLOBAL::SETTINGS.WINDOW_WIDTH = IVGLOBAL::WINDOW_PREVIOUS_WIDTH;
+			IVGLOBAL::SETTINGS.WINDOW_HEIGHT = IVGLOBAL::WINDOW_PREVIOUS_HEIGHT;
 		}
-		//otherwise, write current size
-		else {
-			outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_WIDTH), sizeof(IVGLOBAL::WINDOW_WIDTH));
-			outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_HEIGHT), sizeof(IVGLOBAL::WINDOW_HEIGHT));
-		}
-		//write window state
-		outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::WINDOW_MAXIMIZED), sizeof(IVGLOBAL::WINDOW_MAXIMIZED));
-		//write current theme state
-		outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::DISPLAY_MODE_DARK), sizeof(IVGLOBAL::DISPLAY_MODE_DARK));
 
-
+		outputStream.write(reinterpret_cast<char *>(&IVGLOBAL::SETTINGS), sizeof(IVGLOBAL::SETTINGS));
 		outputStream.close();
 	}
 	else {
@@ -205,47 +206,46 @@ void writeSettings() {
 void redrawImage(Window* win, SDL_Texture* imageTexture) {
 
 	//image is too big for the window
-	if (IVGLOBAL::IMAGE_HEIGHT > IVGLOBAL::WINDOW_HEIGHT || IVGLOBAL::IMAGE_WIDTH > IVGLOBAL::WINDOW_WIDTH) {
+	if (IVGLOBAL::IMAGE_HEIGHT > IVGLOBAL::SETTINGS.WINDOW_HEIGHT || IVGLOBAL::IMAGE_WIDTH > IVGLOBAL::SETTINGS.WINDOW_WIDTH) {
 		//determine the shapes of window and image
 		float imageAspectRatio = IVGLOBAL::IMAGE_WIDTH/(float) IVGLOBAL::IMAGE_HEIGHT;
-		float windowAspectRatio = IVGLOBAL::WINDOW_WIDTH/(float) IVGLOBAL::WINDOW_HEIGHT;
+		float windowAspectRatio = IVGLOBAL::SETTINGS.WINDOW_WIDTH/(float) IVGLOBAL::SETTINGS.WINDOW_HEIGHT;
 
 		//width is priority
 		if (imageAspectRatio > windowAspectRatio) {
 			//figure out how image will be scaled to fit
-			float imageReduction = IVGLOBAL::WINDOW_WIDTH/(float) IVGLOBAL::IMAGE_WIDTH;
+			float imageReduction = IVGLOBAL::SETTINGS.WINDOW_WIDTH/(float) IVGLOBAL::IMAGE_WIDTH;
 			//apply transformation to height
 			int imageTargetHeight = imageReduction * IVGLOBAL::IMAGE_HEIGHT;
 
 			//horizontal adjustment
-			int xPos = (IVGLOBAL::WINDOW_WIDTH - IVGLOBAL::WINDOW_WIDTH * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_X;
+			int xPos = (IVGLOBAL::SETTINGS.WINDOW_WIDTH - IVGLOBAL::SETTINGS.WINDOW_WIDTH * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_X;
 			//vertical adjustment
-			int yPos = (IVGLOBAL::WINDOW_HEIGHT - imageTargetHeight * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_Y;
+			int yPos = (IVGLOBAL::SETTINGS.WINDOW_HEIGHT - imageTargetHeight * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_Y;
 
-			SDL_Rect windowDestination = {xPos, yPos, (int) (IVGLOBAL::WINDOW_WIDTH * IVGLOBAL::VIEWPORT_ZOOM), (int) (imageTargetHeight * IVGLOBAL::VIEWPORT_ZOOM)};
-
+			SDL_Rect windowDestination = {xPos, yPos, (int) (IVGLOBAL::SETTINGS.WINDOW_WIDTH * IVGLOBAL::VIEWPORT_ZOOM), (int) (imageTargetHeight * IVGLOBAL::VIEWPORT_ZOOM)};
 			SDL_RenderCopy(win->renderer, imageTexture, 0, &windowDestination);
 		}
 		//height is priority or equal priority
 		else {
 			//figure out how image will be scaled to fit
-			float imageReduction = IVGLOBAL::WINDOW_HEIGHT/(float) IVGLOBAL::IMAGE_HEIGHT;
+			float imageReduction = IVGLOBAL::SETTINGS.WINDOW_HEIGHT/(float) IVGLOBAL::IMAGE_HEIGHT;
 			//apply transformation to width
 			int imageTargetWidth = imageReduction * IVGLOBAL::IMAGE_WIDTH;
 
 			//horizontal adjustment
-			int xPos = (IVGLOBAL::WINDOW_WIDTH - imageTargetWidth * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_X;
+			int xPos = (IVGLOBAL::SETTINGS.WINDOW_WIDTH - imageTargetWidth * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_X;
 			//vertical adjustment
-			int yPos = (IVGLOBAL::WINDOW_HEIGHT - IVGLOBAL::WINDOW_HEIGHT * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_Y;
+			int yPos = (IVGLOBAL::SETTINGS.WINDOW_HEIGHT - IVGLOBAL::SETTINGS.WINDOW_HEIGHT * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_Y;
 
-			SDL_Rect windowDestination = {xPos, yPos, (int) (imageTargetWidth * IVGLOBAL::VIEWPORT_ZOOM), (int) (IVGLOBAL::WINDOW_HEIGHT * IVGLOBAL::VIEWPORT_ZOOM)};
+			SDL_Rect windowDestination = {xPos, yPos, (int) (imageTargetWidth * IVGLOBAL::VIEWPORT_ZOOM), (int) (IVGLOBAL::SETTINGS.WINDOW_HEIGHT * IVGLOBAL::VIEWPORT_ZOOM)};
 			SDL_RenderCopy(win->renderer, imageTexture, 0, &windowDestination);
 		}
 	}
 	//image will fit in existing window
 	else {
-		int xPos = (IVGLOBAL::WINDOW_WIDTH - IVGLOBAL::IMAGE_WIDTH * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_X;
-		int yPos = (IVGLOBAL::WINDOW_HEIGHT - IVGLOBAL::IMAGE_HEIGHT * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_Y;
+		int xPos = (IVGLOBAL::SETTINGS.WINDOW_WIDTH - IVGLOBAL::IMAGE_WIDTH * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_X;
+		int yPos = (IVGLOBAL::SETTINGS.WINDOW_HEIGHT - IVGLOBAL::IMAGE_HEIGHT * IVGLOBAL::VIEWPORT_ZOOM)/2 + IVGLOBAL::VIEWPORT_Y;
 		SDL_Rect windowDestination = {xPos, yPos, (int) (IVGLOBAL::IMAGE_WIDTH * IVGLOBAL::VIEWPORT_ZOOM), (int) (IVGLOBAL::IMAGE_HEIGHT * IVGLOBAL::VIEWPORT_ZOOM)};
 		SDL_RenderCopy(win->renderer, imageTexture, 0, &windowDestination);
 	}
@@ -257,8 +257,8 @@ void redrawImage(Window* win, SDL_Texture* imageTexture) {
 * tileTexture 		> Texture as SDL_Texture to tile
 */
 void drawTileTexture(Window* win, SDL_Texture* tileTexture) {
-	for (int h = 0; h < IVGLOBAL::WINDOW_HEIGHT; h += IVCONST::TEXTURE_CHECKERBOARD_RESOLUTION){
-		for (int w = 0; w < IVGLOBAL::WINDOW_WIDTH; w += IVCONST::TEXTURE_CHECKERBOARD_RESOLUTION){
+	for (int h = 0; h < IVGLOBAL::SETTINGS.WINDOW_HEIGHT; h += IVCONST::TEXTURE_CHECKERBOARD_RESOLUTION){
+		for (int w = 0; w < IVGLOBAL::SETTINGS.WINDOW_WIDTH; w += IVCONST::TEXTURE_CHECKERBOARD_RESOLUTION){
 			SDL_Rect destination = {w, h, IVCONST::TEXTURE_CHECKERBOARD_RESOLUTION, IVCONST::TEXTURE_CHECKERBOARD_RESOLUTION};
 			SDL_RenderCopy(win->renderer, tileTexture, 0, &destination);
 		}
@@ -373,7 +373,7 @@ int main(int argc, char* argv[]) {
 	readSettings();
 
 	/* Create invisible application window */
-	Window win(IVGLOBAL::WINDOW_WIDTH, IVGLOBAL::WINDOW_HEIGHT, IVGLOBAL::WINDOW_X, IVGLOBAL::WINDOW_Y, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | (IVGLOBAL::WINDOW_MAXIMIZED ? SDL_WINDOW_MAXIMIZED : 0));
+	Window win(IVGLOBAL::SETTINGS.WINDOW_WIDTH, IVGLOBAL::SETTINGS.WINDOW_HEIGHT, IVGLOBAL::SETTINGS.WINDOW_X, IVGLOBAL::SETTINGS.WINDOW_Y, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | (IVGLOBAL::SETTINGS.WINDOW_MAXIMIZED ? SDL_WINDOW_MAXIMIZED : 0));
 	win.setTitle(IVCONST::APPLICATION_TITLE.c_str());
 
 	//no file passed in
@@ -423,7 +423,7 @@ int main(int argc, char* argv[]) {
 	SDL_FreeSurface(transparency_tmp_l);
 
 	//draw background texture before continuing to show program is loading
-	draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+	draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 
 	//load up the image passed in
 	if (loadTextureFromFile(std::string(argv[1]), &win, &imageTexture)) {
@@ -448,7 +448,7 @@ int main(int argc, char* argv[]) {
 	win.setTitle((IVGLOBAL::PATH_FILE_IMAGE.filename().string() + " - " + IVCONST::APPLICATION_TITLE).c_str());
 
 	//finally draw image and background
-	draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+	draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 
 	//iterate image folder and mark position of currently open image
 	int pos = 0;
@@ -486,21 +486,21 @@ int main(int argc, char* argv[]) {
 							quit = true;
 							break;
 						case SDL_WINDOWEVENT_MOVED:
-							IVGLOBAL::WINDOW_MAXIMIZED = true;
-							IVGLOBAL::WINDOW_PREVIOUS_X = IVGLOBAL::WINDOW_X;
-							IVGLOBAL::WINDOW_PREVIOUS_Y = IVGLOBAL::WINDOW_Y;
-							SDL_GetWindowPosition(win.window, &IVGLOBAL::WINDOW_X, &IVGLOBAL::WINDOW_Y);
+							IVGLOBAL::WINDOW_MOVED = true;
+							IVGLOBAL::WINDOW_PREVIOUS_X = IVGLOBAL::SETTINGS.WINDOW_X;
+							IVGLOBAL::WINDOW_PREVIOUS_Y = IVGLOBAL::SETTINGS.WINDOW_Y;
+							SDL_GetWindowPosition(win.window, &IVGLOBAL::SETTINGS.WINDOW_X, &IVGLOBAL::SETTINGS.WINDOW_Y);
 							break;
 						case SDL_WINDOWEVENT_MAXIMIZED:
-							IVGLOBAL::WINDOW_MAXIMIZED = false;
-							IVGLOBAL::WINDOW_MAXIMIZED = true;
+							IVGLOBAL::WINDOW_MOVED = false;
+							IVGLOBAL::SETTINGS.WINDOW_MAXIMIZED = true;
 							break;
 						case SDL_WINDOWEVENT_SIZE_CHANGED:
-							IVGLOBAL::WINDOW_MAXIMIZED = false;
-							IVGLOBAL::WINDOW_PREVIOUS_WIDTH = IVGLOBAL::WINDOW_WIDTH;
-							IVGLOBAL::WINDOW_PREVIOUS_HEIGHT = IVGLOBAL::WINDOW_HEIGHT;
-							SDL_GetWindowSize(win.window, &IVGLOBAL::WINDOW_WIDTH, &IVGLOBAL::WINDOW_HEIGHT);
-							draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+							IVGLOBAL::SETTINGS.WINDOW_MAXIMIZED = false;
+							IVGLOBAL::WINDOW_PREVIOUS_WIDTH = IVGLOBAL::SETTINGS.WINDOW_WIDTH;
+							IVGLOBAL::WINDOW_PREVIOUS_HEIGHT = IVGLOBAL::SETTINGS.WINDOW_HEIGHT;
+							SDL_GetWindowSize(win.window, &IVGLOBAL::SETTINGS.WINDOW_WIDTH, &IVGLOBAL::SETTINGS.WINDOW_HEIGHT);
+							draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 							break;
 					}
 					break;
@@ -508,22 +508,22 @@ int main(int argc, char* argv[]) {
 					switch (sdlEvent.key.keysym.sym) {
 						case SDLK_KP_PLUS: //keypad +, zoom in
 							IVGLOBAL::VIEWPORT_ZOOM = std::min(IVCONST::ZOOM_MAX, IVGLOBAL::VIEWPORT_ZOOM * 2.0f);
-							draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+							draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 							break;
 						case SDLK_KP_MINUS: //keypad -, zoom out
 							IVGLOBAL::VIEWPORT_ZOOM = std::max(IVCONST::ZOOM_MIN, IVGLOBAL::VIEWPORT_ZOOM/2.0f);
-							draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+							draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 							break;
 						case SDLK_KP_0: //keypad 0, reset zoom and positioning
 							resetViewport();
-							draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+							draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 							break;
 						case SDLK_ESCAPE:
 							quit = true;
 							break;
 						case SDLK_TAB:
-							IVGLOBAL::DISPLAY_MODE_DARK = !IVGLOBAL::DISPLAY_MODE_DARK;
-							draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+							IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK = !IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK;
+							draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 							break;
 						case SDLK_DELETE:
 							if (IDYES == MessageBox(nullptr, "Are you sure you want to permanently delete this image?\nThis action cannot be reversed!", "Delete Image", MB_YESNO | MB_DEFBUTTON2 | MB_ICONEXCLAMATION)) {
@@ -556,7 +556,7 @@ int main(int argc, char* argv[]) {
 								break;
 							}
 							resetViewport(); //new image so reset zoom and positioning
-							draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+							draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 							break;
 						//next image
 						case SDLK_RIGHT: //move forward
@@ -569,7 +569,7 @@ int main(int argc, char* argv[]) {
 								break;
 							}
 							resetViewport(); //new image so reset zoom and positioning
-							draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+							draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 							break;
 					}
 					break;
@@ -583,7 +583,7 @@ int main(int argc, char* argv[]) {
 						IVGLOBAL::VIEWPORT_X *= IVCONST::ZOOM_SCROLL_SENSITIVITY;
 						IVGLOBAL::VIEWPORT_Y *= IVCONST::ZOOM_SCROLL_SENSITIVITY;
 
-						draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+						draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 					}
 					else if (sdlEvent.wheel.y < 0) {
 						IVGLOBAL::VIEWPORT_ZOOM = std::max(IVCONST::ZOOM_MIN, IVGLOBAL::VIEWPORT_ZOOM / IVCONST::ZOOM_SCROLL_SENSITIVITY);
@@ -592,7 +592,7 @@ int main(int argc, char* argv[]) {
 						IVGLOBAL::VIEWPORT_X /= IVCONST::ZOOM_SCROLL_SENSITIVITY;
 						IVGLOBAL::VIEWPORT_Y /= IVCONST::ZOOM_SCROLL_SENSITIVITY;
 
-						draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+						draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 					}
 					break;
 				case SDL_MOUSEBUTTONDOWN:
@@ -603,7 +603,7 @@ int main(int argc, char* argv[]) {
 							break;
 						case SDL_BUTTON_MIDDLE:
 							resetViewport();
-							draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+							draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 							break;
 						case SDL_BUTTON_X1:
 							//bind mouse back button to left arrow to return to previous image
@@ -639,7 +639,7 @@ int main(int argc, char* argv[]) {
 						SDL_GetMouseState(&mouseX, &mouseY);
 						IVGLOBAL::VIEWPORT_X += mouseX - mousePreviousX;
 						IVGLOBAL::VIEWPORT_Y += mouseY - mousePreviousY;
-						draw(&win, (IVGLOBAL::DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
+						draw(&win, (IVGLOBAL::SETTINGS.DISPLAY_MODE_DARK) ? IVGLOBAL::TEXTURE_TRANSPARENCY_DARK : IVGLOBAL::TEXTURE_TRANSPARENCY_LIGHT, imageTexture);
 					}
 					break;
 
