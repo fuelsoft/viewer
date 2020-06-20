a51dedfe890ba87f9c250b62d42c0e3068d94570
Nick <fuelsoft@users.noreply.github.com>
2020-06-20 16:20:07 -0700

    Almost complete HEIF support Switched colour conversion to library call instead of doing it manually

--- subclasses/IVStaticImage.cpp
+++ subclasses/IVStaticImage.cpp
@@ -16,81 +16,50 @@ IVStaticImage::IVStaticImage(SDL_Renderer* renderer, std::filesystem::path path)
 	if (filetype == IVUTIL::TYPE_SDL) {
 		//load SDL image
 		surface = IMG_Load(path.string().c_str());
+
+		if (!surface) {
+			std::cout << IVUTIL::LOG_ERROR << "COULD NOT CREATE SURFACE" << std::endl;
+			throw IVUTIL::EXCEPT_IMG_LOAD_FAIL;
+		}
 	}
 	else if (filetype == IVUTIL::TYPE_LIBHEIF) {
 		//load HEIF image 
 		heif::Context ctx;
 		ctx.read_from_file(path.string());
-
 		heif::ImageHandle handle = ctx.get_primary_image_handle();
 
-		heif::Image img = handle.decode_image(heif_colorspace_undefined, heif_chroma_undefined);
+		//loading as interleaved RGB will force an automatic conversion from RGB(A)/YCbCr to RGBA
+		heif::Image img = handle.decode_image(heif_colorspace_RGB, heif_chroma_interleaved_RGBA);
 
-		if (img.has_channel(heif_channel_R)
-		 && img.has_channel(heif_channel_G)
-		 && img.has_channel(heif_channel_B)) { //RGB mode
+		if (img.has_channel(heif_channel_interleaved)) {
+			int im_w = img.get_width(heif_channel_interleaved);
+			int im_h = img.get_height(heif_channel_interleaved);
+			int im_d = img.get_bits_per_pixel(heif_channel_interleaved); //depth
+			int im_p; //libheif calls this 'stride', SDL calls it 'pitch', unused
 
-		 	surface = SDL_CreateRGBSurface(0,
-				img.get_width(heif_channel_G), 
-				img.get_height(heif_channel_G), 
-				32, //BPP
-				0x000000FF, //RGBA
-				0x0000FF00, 
-				0x00FF0000, 
-				0xFF000000);
-
-			/* TODO: RGB MODE */
-
-		 	std::cout << IVUTIL::LOG_NOTICE << "Detected RGB HEIF" << std::endl;
-		}
-		else if (img.has_channel(heif_channel_Y)
-			  && img.has_channel(heif_channel_Cb)
-			  && img.has_channel(heif_channel_Cr)) { //YCbCr mode
-
-			int im_w = img.get_width(heif_channel_Y);
-			int im_h = img.get_height(heif_channel_Y);
-			// int im_d = img.get_bits_per_pixel(heif_channel_Y); //not yet used, silence compiler
-			int im_p;
-
-			surface = SDL_CreateRGBSurface(0,
+			surface = SDL_CreateRGBSurfaceWithFormat(0,
 				im_w, 
 				im_h, 
-				32, //BPP
-				0x000000FF, //RGBA
-				0x0000FF00, 
-				0x00FF0000, 
-				0xFF000000);
+				im_d,
+				SDL_PIXELFORMAT_RGBA32);
 
-			/* TODO: ALPHA */
-			uint8_t* image_Y_data = img.get_plane(heif_channel_Y, &im_p);
-
-			//for now, display Y channel, B&W data
-			//colour math is complicated, I'll do it later
-			/* TODO: MATH */
-			for (int i = 0; i < im_w * im_h; i++) {
-				int lum = image_Y_data[i];
-				((uint8_t *) surface->pixels)[i * 4] 	 = lum;
-				((uint8_t *) surface->pixels)[i * 4 + 1] = lum;
-				((uint8_t *) surface->pixels)[i * 4 + 2] = lum;
-				((uint8_t *) surface->pixels)[i * 4 + 3] = 0xFF;
+			if (!surface) {
+				std::cout << IVUTIL::LOG_ERROR << "COULD NOT ALLOCATE SURFACE" << std::endl;
+				throw IVUTIL::EXCEPT_IMG_LOAD_FAIL;
 			}
 
-		 	std::cout << IVUTIL::LOG_NOTICE << "Detected YCbCr HEIF" << std::endl;
+		 	uint8_t* RGBA = img.get_plane(heif_channel_interleaved, &im_p); //pointer to pixel data
+
+		 	memcpy(surface->pixels, (void *) RGBA, im_w * im_h * (im_d >> 3)); //copy to SDL Surface
 		}
 		else {
+			std::cout << IVUTIL::LOG_ERROR << "UNSUPPORTED COLOUR FORMAT" << std::endl;
 			throw IVUTIL::EXCEPT_IMG_LOAD_FAIL;
 		}
-		/* TODO: Channel size checking */
-
-		std::cout << "Success!" << std::endl;
-
 	}
 	else {
 		//not a supported format
-		throw IVUTIL::EXCEPT_IMG_LOAD_FAIL;
-	}
-
-	if (!surface) {
+		std::cout << IVUTIL::LOG_ERROR << "UNSUPPORTED IMAGE FORMAT" << std::endl;
 		throw IVUTIL::EXCEPT_IMG_LOAD_FAIL;
 	}
 
